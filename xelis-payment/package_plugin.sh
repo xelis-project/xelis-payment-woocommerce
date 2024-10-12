1a00fcbf8c38993b2d89c3eb8669c9374ebe10f4
g45t345rt <49697006+g45t345rt@users.noreply.github.com>
2024-10-12 12:05:54 -0400

    add plugin packaging script

---
+++ xelis-payment/package_plugin.sh
@@ -0,0 +1,28 @@
+cd block
+
+npm run build
+
+cd ../
+
+PATHS=(
+  "./xelis_wallet.php"
+  "./xelis_wallet_logs_page.php"
+  "./xelis_rest.php"
+  "./xelis_payment.php"
+  "./xelis_payment_state.php"
+  "./xelis_payment_method.php"
+  "./xelis_payment_gateway.php"
+  "./xelis_package.php"
+  "./xelis_node.php"
+  "./xelis_data.php"
+  "./block/style.css"
+  "./block/build/index.js"
+  "./block/require.js"
+  "./assets"
+)
+
+OUTPUT="xelis_payment.zip"
+
+zip -r "$OUTPUT" "${PATHS[@]}"
+
+echo "Package created!"
