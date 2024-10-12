d3ce16cf1faf9743ff53f7fdf12b91e33068da9e
g45t345rt <49697006+g45t345rt@users.noreply.github.com>
2024-10-12 15:16:55 -0400

    add wallet transaction list filtering

--- xelis-payment/xelis_wallet_page.php
+++ xelis-payment/xelis_wallet_page.php
@@ -25,32 +25,98 @@ function render_page()
   $balance = $xelis_wallet->shift_xel($balance_atomic);
   $logs = $xelis_wallet->get_output();
 
-  if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $amount = $_POST['amount'];
-    $asset = $_POST['asset'];
-    $destination = $_POST['destination'];
+  $filter_address = null;
+  $accept_incoming = true;
+  $accept_outgoing = true;
+  $accept_coinbase = true;
+  $accept_burn = true;
+  $filter_type = 'all';
 
-    try {
-      $amount_atomic = $xelis_wallet->unshift_xel($amount);
-      $tx = $xelis_wallet->send_funds($amount_atomic, $asset, $destination);
-      $send_funds_msg = $amount . " sent to " . $destination . " - " . $tx->hash;
-    } catch (Exception $e) {
-      $send_funds_err_msg = $e->getMessage();
+  if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (isset($_POST["send_funds"])) {
+      $amount = $_POST['amount'];
+      $asset = $_POST['asset'];
+      $destination = $_POST['destination'];
+
+      try {
+        $amount_atomic = $xelis_wallet->unshift_xel($amount);
+        $tx = $xelis_wallet->send_funds($amount_atomic, $asset, $destination);
+        $send_funds_msg = $amount . " sent to " . $destination . " - " . $tx->hash;
+      } catch (Exception $e) {
+        $send_funds_err_msg = $e->getMessage();
+      }
+    }
+
+    if (isset($_POST["filter_transactions"])) {
+      $filter_address = $_POST["address"];
+      $filter_type = $_POST["type"];
+
+      switch ($filter_type) {
+        case "all":
+          // do nothing 
+          break;
+        case "incoming":
+          $accept_incoming = true;
+          $accept_outgoing = false;
+          $accept_coinbase = false;
+          $accept_burn = false;
+          break;
+        case "outgoing":
+          $accept_incoming = false;
+          $accept_outgoing = true;
+          $accept_coinbase = false;
+          $accept_burn = false;
+          break;
+        case "coinbase":
+          $accept_incoming = false;
+          $accept_outgoing = false;
+          $accept_coinbase = true;
+          $accept_burn = false;
+          break;
+        case "burn":
+          $accept_incoming = false;
+          $accept_outgoing = false;
+          $accept_coinbase = false;
+          $accept_burn = true;
+          break;
+      }
+    }
+
+    if (isset($_POST["rescan"])) {
+
     }
   }
 
   try {
     $txs = $xelis_wallet->get_transactions(
       0,
-      true,
-      true,
-      true,
-      true
+      $accept_incoming,
+      $accept_outgoing,
+      $accept_coinbase,
+      $accept_burn,
+      $filter_address
     );
   } catch (Exception $e) {
   }
 
   ?>
+  <style>
+    table,
+    td,
+    th {
+      border: .1rem solid;
+      padding: .5rem;
+    }
+
+    table th {
+      text-align: left;
+    }
+
+    table {
+      width: 100%;
+      border-collapse: collapse;
+    }
+  </style>
   <div class="wrap">
     <h1><?php esc_html_e('XELIS Wallet', 'xelis_payment'); ?></h1>
     <a href="/wp-admin/admin.php?page=wc-settings&tab=checkout&section=xelis_payment">Go to XELIS Payment settings</a>
@@ -59,7 +125,7 @@ function render_page()
     <h2>Send funds</h2>
     <form method="post" action="">
       <label for="amount">Amount:</label><br>
-      <input type="number" id="amount" name="amount" required>
+      <input type="text" id="amount" name="amount" required>
       <br>
       <label for="asset">Asset:</label><br>
       <input type="text" id="asset" value="<?php echo Xelis_Wallet::$XELIS_ASSET ?>" name="asset" required>
@@ -67,7 +133,7 @@ function render_page()
       <label for="destination">Destination:</label><br>
       <input type="text" id="destination" name="destination" required>
       <br><br>
-      <input type="submit" value="Submit">
+      <input type="submit" name="send_funds" value="Submit">
     </form>
     <?php
     if (isset($send_funds_msg)) {
@@ -84,16 +150,39 @@ function render_page()
     }
     ?>
     <h2>Transactions</h2>
+    <div>
+      <form method="post" action="">
+        <input type="text" name="address" value="<?php echo $filter_address ?>" placeholder="Filter by address" />
+        <select name="type">
+          <option value="all" <?php if ($filter_type === 'all')
+            echo 'selected'; ?>>All types</option>
+          <option value="incoming" <?php if ($filter_type === 'incoming')
+            echo 'selected'; ?>>Incoming</option>
+          <option value="outgoing" <?php if ($filter_type === 'outgoing')
+            echo 'selected'; ?>>Outgoing</option>
+          <option value="coinbase" <?php if ($filter_type === 'coinbase')
+            echo 'selected'; ?>>Coinbase</option>
+          <option value="burn" <?php if ($filter_type === 'burn')
+            echo 'selected'; ?>>Burn</option>
+        </select>
+        <input type="submit" name="filter_transactions" value="Filter">
+      </form>
+      <form method="post" action="">
+        <input type="submit" name="rescan" value="Rescan">
+      </form>
+    </div>
     <?php if (!empty($txs)): ?>
       <table>
         <tr>
           <th>Tx ID</th>
+          <th>Topoheight</th>
           <th>Type</th>
           <th>Transfers</th>
         </tr>
         <?php foreach ($txs as $tx): ?>
           <tr>
             <td><?php echo esc_html($tx->hash); ?></td>
+            <td><?php echo $tx->topoheight ?></td>
             <?php if (isset($tx->incoming)): ?>
               <td>incoming</td>
               <td>
@@ -108,25 +197,27 @@ function render_page()
             <?php endif; ?>
           </tr>
           <?php if (isset($tx->incoming)): ?>
-            <?php foreach ($tx->incoming->transfers as $transfer): ?>
+            <?php foreach ($tx->incoming->transfers as $idx=>$transfer): ?>
               <tr>
-                <td colspan="3">
-                  <?php echo esc_html($transfer->amount); ?>
-                  <?php echo esc_html($tx->incoming->from); ?>
-                  <?php echo esc_html($transfer->extra_data); ?>
-                  <?php echo esc_html($transfer->asset); ?>
+                <td colspan="4">
+                  <?php echo $idx+1 ?>.
+                  Amount: <?php echo esc_html($transfer->amount); ?>
+                  From: <?php echo esc_html($tx->incoming->from); ?>
+                  Extra Data: <?php echo esc_html($transfer->extra_data); ?>
+                  Asset: <?php echo esc_html($transfer->asset); ?>
                 </td>
               </tr>
             <?php endforeach; ?>
           <?php endif; ?>
           <?php if (isset($tx->outgoing)): ?>
-            <?php foreach ($tx->outgoing->transfers as $transfer): ?>
+            <?php foreach ($tx->outgoing->transfers as $idx=>$transfer): ?>
               <tr>
-                <td colspan="3">
-                  <?php echo esc_html($transfer->amount); ?>
-                  <?php echo esc_html($transfer->destination); ?>
-                  <?php echo esc_html($transfer->extra_data); ?>
-                  <?php echo esc_html($transfer->asset); ?>
+                <td colspan="4">
+                  <?php echo $idx+1 ?>.
+                  Amount: <?php echo esc_html($transfer->amount); ?>
+                  Destination: <?php echo esc_html($transfer->destination); ?>
+                  Extra Data: <?php echo esc_html($transfer->extra_data); ?>
+                  Asset: <?php echo esc_html($transfer->asset); ?>
                 </td>
               </tr>
             <?php endforeach; ?>
@@ -137,13 +228,13 @@ function render_page()
       <p>No transactions.</p>
     <?php endif; ?>
     <h2>Logs</h2>
-    <textarea style="width: 100%;" rows="10">
-          <?php
-          foreach ($logs as $line) {
-            echo htmlspecialchars($line) . "\n";
-          }
-          ?>
-        </textarea>
+    <?php
+    echo '<textarea style="width: 100%;" rows="10">';
+    foreach ($logs as $line) {
+      echo htmlspecialchars($line) . "\n";
+    }
+    echo ' </textarea>';
+    ?>
   </div>
   <?php
 }
